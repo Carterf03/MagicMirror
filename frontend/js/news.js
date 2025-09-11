f7d003cecb61ad756e515cf8884693fcf294aeee
Carterf03 <Carterfultz03@gmail.com>
2025-09-11 19:42:44 -0400

    news.js documentation

--- frontend/js/news.js
+++ frontend/js/news.js
@@ -10,8 +10,12 @@ import api from './APIClient.js';
 //     });
 // });
 
+// HTML Section for Events
 const eventsSection = document.querySelector('section#events');
+// API call to get top news stories for a specific category
 api.getNewsStories("politics").then(stories => {
+    // Creates a HTML div and p tag for the news headline
+    // and adds it to the Event Section
     stories.forEach(story => {
         const div = document.createElement('div');
         const text = document.createElement('p');

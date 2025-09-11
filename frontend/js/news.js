eefd4fe7c1cb96feadac06c706f298fefaa1ec5c
Carterf03 <Carterfultz03@gmail.com>
2025-09-10 21:38:38 -0400

    Updated the new JS to display the retrieved news stories

--- frontend/js/news.js
+++ frontend/js/news.js
@@ -1,10 +1,25 @@
 import api from './APIClient.js';
 
-const newsbutton = document.querySelector('#newsbutton');
-newsbutton.addEventListener('click', e => {
-    api.getNewsStories("technology").then(stories => {
-        console.log(stories);
-    }).catch(err => {
-        console.log(err);
+// TESTING
+// const newsbutton = document.querySelector('#newsbutton');
+// newsbutton.addEventListener('click', e => {
+//     api.getNewsStories("technology").then(stories => {
+//         console.log(stories);
+//     }).catch(err => {
+//         console.log(err);
+//     });
+// });
+
+const eventsSection = document.querySelector('section#events');
+api.getNewsStories("politics").then(stories => {
+    stories.forEach(story => {
+        const div = document.createElement('div');
+        const text = document.createElement('p');
+        text.textContent = story.title;
+        // div.classList.add();
+        div.appendChild(text);
+        eventsSection.appendChild(div);
     });
+}).catch(err => {
+    console.log(err);
 });

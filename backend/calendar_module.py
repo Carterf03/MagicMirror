b549373d95ac166c8c08067980a80ad400c2be05
Carterf03 <Carterfultz03@gmail.com>
2026-03-01 15:17:38 -0500

    Updated calendar_module.py

--- backend/calendar_module.py
+++ backend/calendar_module.py
@@ -1,9 +1,10 @@
-import datetime
 import os.path
 
-from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
